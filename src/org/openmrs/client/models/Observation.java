eaba49f78f3e9247e87c4d6f722a53972f863388
Kamil Kalfas <kkalfas@soldevelo.com>
2014-09-12 16:42:46 +0200

    AC-10: Extended Visit definition and mapper.

---
+++ src/org/openmrs/client/models/Observation.java
@@ -0,0 +1,42 @@
+package org.openmrs.client.models;
+
+import java.io.Serializable;
+
+public class Observation implements Serializable {
+    private Long id;
+    private String uuid;
+    private String display;
+    private String displayValue;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
+
+    public void setDisplayValue(String displayValue) {
+        this.displayValue = displayValue;
+    }
+}
--- src/org/openmrs/client/models/mappers/VisitMapper.java
+++ src/org/openmrs/client/models/mappers/VisitMapper.java
@@ -1,9 +1,15 @@
 package org.openmrs.client.models.mappers;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.openmrs.client.models.Encounter;
+import org.openmrs.client.models.Observation;
 import org.openmrs.client.models.Visit;
 import org.openmrs.client.utilities.DateUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class VisitMapper {
 
@@ -17,6 +23,34 @@ public final class VisitMapper {
         visit.setVisitType(jsonObject.getJSONObject("visitType").getString("display"));
         visit.setStartDate(DateUtils.convertTime(jsonObject.getString("startDatetime")));
         visit.setStopDate(DateUtils.convertTime(jsonObject.getString("stopDatetime")));
+        JSONArray encountersJSONArray = jsonObject.getJSONArray("encounters");
+        List<Encounter> encounterList = new ArrayList<Encounter>();
+        for (int i = 0; i < encountersJSONArray.length(); i++) {
+            Encounter encounter = new Encounter();
+            JSONObject encounterJSONObject = encountersJSONArray.getJSONObject(i);
+            List<Observation> observationList = new ArrayList<Observation>();
+            encounter.setDisplay(encounterJSONObject.getString("display"));
+            encounter.setUuid(encounterJSONObject.getString("uuid"));
+            encounter.setEncounterType(Encounter.EncounterType.getType(encounterJSONObject.getJSONObject("encounterType").getString("display")));
+            encounter.setEncounterDatetime(DateUtils.convertTime(encounterJSONObject.getString("encounterDatetime")));
+            JSONArray obsJSONArray = encounterJSONObject.getJSONArray("obs");
+            for (int j = 0; j < obsJSONArray.length(); j++) {
+                Observation observation = new Observation();
+                JSONObject observationJSONObject = obsJSONArray.getJSONObject(j);
+                observation.setUuid(observationJSONObject.getString("uuid"));
+                if (Encounter.EncounterType.VITALS.equals(encounter.getEncounterType())) {
+                    String[] labelAndValue = observationJSONObject.getString("display").split(":");
+                    observation.setDisplay(labelAndValue[0]);
+                    observation.setDisplayValue(labelAndValue[1]);
+                } else {
+                    observation.setDisplay(observationJSONObject.getString("display"));
+                }
+                observationList.add(observation);
+            }
+            encounter.setObservations(observationList);
+            encounterList.add(encounter);
+        }
+        visit.setEncounters(encounterList);
         return visit;
     }
 }

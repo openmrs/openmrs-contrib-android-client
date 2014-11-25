bb418ec0242fd6b334d834c7c377930a9f624d4a
Emilia Haligowska <ehaligowska@soldevelo.com>
2014-11-25 16:01:46 +0100

    AC-76_2 Deleted unused import

--- src/org/openmrs/client/activities/PatientDashboardActivity.java
+++ src/org/openmrs/client/activities/PatientDashboardActivity.java
@@ -15,7 +15,6 @@
 package org.openmrs.client.activities;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;

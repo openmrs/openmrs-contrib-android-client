083dd5b041e4d9684f21ee21b2f5a473fc1a0924
Avijit Ghosh <avijitg22@gmail.com>
2016-02-19 23:31:35 +0530

    Added error logging instead of stack trace

--- openmrs-client/src/main/java/org/openmrs/mobile/net/AuthorizationManager.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/net/AuthorizationManager.java
@@ -42,7 +42,7 @@ public class AuthorizationManager extends BaseManager {
 
             @Override
             public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
+                mLogger.i(error.toString());
             }
         };
 

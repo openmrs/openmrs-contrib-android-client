3b03205fff5157e717a24a9ea3716094232d7e59
brainbreaker <gautamprajapati06@gmail.com>
2016-03-03 04:44:26 +0530

    Fixed AC-131

--- openmrs-client/src/main/java/org/openmrs/mobile/models/mappers/VisitMapper.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/models/mappers/VisitMapper.java
@@ -34,11 +34,24 @@ public final class VisitMapper {
 
     public static Visit map(JSONObject jsonObject) throws JSONException {
         Visit visit = new Visit();
-        visit.setUuid(jsonObject.getString("uuid"));
-        visit.setVisitPlace(jsonObject.getJSONObject("location").getString(DISPLAY_KEY));
-        visit.setVisitType(jsonObject.getJSONObject("visitType").getString(DISPLAY_KEY));
-        visit.setStartDate(DateUtils.convertTime(jsonObject.getString("startDatetime")));
-        visit.setStopDate(DateUtils.convertTime(jsonObject.getString("stopDatetime")));
+        if (jsonObject.has("uuid")) {
+            visit.setUuid(jsonObject.getString("uuid"));
+        }
+        if (jsonObject.has("location") && !jsonObject.isNull("location")) {
+            JSONObject locationJSONObject = jsonObject.getJSONObject("location");
+            if (locationJSONObject.has(DISPLAY_KEY)) {
+                visit.setVisitPlace(locationJSONObject.getString(DISPLAY_KEY));
+            }
+        }
+        if (jsonObject.has("visitType")) {
+            visit.setVisitType(jsonObject.getJSONObject("visitType").getString(DISPLAY_KEY));
+        }
+        if (jsonObject.has("startDatetime")) {
+            visit.setStartDate(DateUtils.convertTime(jsonObject.getString("startDatetime")));
+        }
+        if (jsonObject.has("stopDatetime")) {
+            visit.setStopDate(DateUtils.convertTime(jsonObject.getString("stopDatetime")));
+        }
         List<Encounter> encounterList = new ArrayList<Encounter>();
 
         if (!jsonObject.isNull("encounters")) {
@@ -47,28 +60,42 @@ public final class VisitMapper {
                 Encounter encounter = new Encounter();
                 JSONObject encounterJSONObject = encountersJSONArray.getJSONObject(i);
                 List<Observation> observationList = new ArrayList<Observation>();
-                encounter.setDisplay(encounterJSONObject.getString(DISPLAY_KEY));
-                encounter.setUuid(encounterJSONObject.getString("uuid"));
-                encounter.setEncounterType(Encounter.EncounterType.getType(encounterJSONObject.getJSONObject("encounterType").getString(DISPLAY_KEY)));
-                encounter.setEncounterDatetime(DateUtils.convertTime(encounterJSONObject.getString("encounterDatetime")));
-                JSONArray obsJSONArray = encounterJSONObject.getJSONArray("obs");
-                for (int j = 0; j < obsJSONArray.length(); j++) {
-                    Observation observation = new Observation();
-                    JSONObject observationJSONObject = obsJSONArray.getJSONObject(j);
-                    observation.setUuid(observationJSONObject.getString("uuid"));
-                    if (Encounter.EncounterType.VITALS.equals(encounter.getEncounterType())) {
-                        String[] labelAndValue = observationJSONObject.getString(DISPLAY_KEY).split(":");
-                        observation.setDisplay(labelAndValue[0]);
-                        observation.setDisplayValue(labelAndValue[1]);
-                    } else if (Encounter.EncounterType.VISIT_NOTE.equals(encounter.getEncounterType())) {
-                        observation = ObservationMapper.diagnosisMap(observationJSONObject);
-                    } else {
-                        observation.setDisplay(observationJSONObject.getString(DISPLAY_KEY));
-                    }
-                    observationList.add(observation);
+                if (encounterJSONObject.has(DISPLAY_KEY)) {
+                    encounter.setDisplay(encounterJSONObject.getString(DISPLAY_KEY));
                 }
-                encounter.setObservations(observationList);
-                encounterList.add(encounter);
+                if (encounterJSONObject.has("uuid")) {
+                    encounter.setUuid(encounterJSONObject.getString("uuid"));
+                }
+                if (encounterJSONObject.has("encounterType")) {
+                    encounter.setEncounterType(Encounter.EncounterType.getType(encounterJSONObject.getJSONObject("encounterType").getString(DISPLAY_KEY)));
+                }
+                if (encounterJSONObject.has("encounterDatetime")) {
+                    encounter.setEncounterDatetime(DateUtils.convertTime(encounterJSONObject.getString("encounterDatetime")));
+                }
+                if (encounterJSONObject.has("obs")) {
+                    JSONArray obsJSONArray = encounterJSONObject.getJSONArray("obs");
+                    for (int j = 0; j < obsJSONArray.length(); j++) {
+                        Observation observation = new Observation();
+                        JSONObject observationJSONObject = obsJSONArray.getJSONObject(j);
+                        if (observationJSONObject.has("uuid")) {
+                            observation.setUuid(observationJSONObject.getString("uuid"));
+                        }
+                        if (Encounter.EncounterType.VITALS.equals(encounter.getEncounterType())) {
+                            if (observationJSONObject.has(DISPLAY_KEY)) {
+                                String[] labelAndValue = observationJSONObject.getString(DISPLAY_KEY).split(":");
+                                observation.setDisplay(labelAndValue[0]);
+                                observation.setDisplayValue(labelAndValue[1]);
+                            }
+                        } else if (Encounter.EncounterType.VISIT_NOTE.equals(encounter.getEncounterType())) {
+                            observation = ObservationMapper.diagnosisMap(observationJSONObject);
+                        } else {
+                            observation.setDisplay(observationJSONObject.getString(DISPLAY_KEY));
+                        }
+                        observationList.add(observation);
+                    }
+                    encounter.setObservations(observationList);
+                    encounterList.add(encounter);
+                }
             }
         }
         visit.setEncounters(encounterList);

c788673336caaffc4a734a7845ce990a423891c2
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2016-06-08 12:59:54 +0200

    Refactored to use deferred and promise

---
+++ openmrs-client/src/main/java/org/openmrs/mobile/api/SimplePromise.java
@@ -0,0 +1,11 @@
+package org.openmrs.mobile.api;
+
+import org.jdeferred.*;
+
+/**
+ * Promise interface to observe when some action has occurred on the corresponding {@link Deferred} object.
+ *
+ * @param <T> Type used for {@link #done(org.jdeferred.DoneCallback)}
+ */
+public interface SimplePromise<T> extends Promise<T, Throwable, Void> {
+}

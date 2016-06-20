6bb66fa354699d0bfe75f5313d12fddbfaeee738
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2016-06-20 13:17:42 +0200

    Introduced Notifier

---
+++ openmrs-client/src/main/java/org/openmrs/mobile/api/Notifier.java
@@ -0,0 +1,15 @@
+package org.openmrs.mobile.api;
+
+import android.widget.Toast;
+
+import org.openmrs.mobile.application.OpenMRS;
+
+/**
+ * Should be used for all notifications displayed to the user.
+ */
+public class Notifier {
+
+    public static void notify(String message) {
+        Toast.makeText(OpenMRS.getInstance(), message, Toast.LENGTH_SHORT).show();
+    }
+}

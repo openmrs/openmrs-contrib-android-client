2886fb7802c7e1f2dd0a71b7b8489dc7163e50e7
Tomasz Marzeion <marzeion.tomasz@gmail.com>
2016-10-25 15:27:55 +0200

    Added license header to all files

--- openmrs-client/src/main/java/org/openmrs/mobile/api/retrofit/LocationApi.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/api/retrofit/LocationApi.java
@@ -1,10 +1,23 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
 package org.openmrs.mobile.api.retrofit;
 
 import org.openmrs.mobile.api.RestApi;
 import org.openmrs.mobile.api.RestServiceBuilder;
 import org.openmrs.mobile.api.promise.SimpleDeferredObject;
 import org.openmrs.mobile.api.promise.SimplePromise;
-import org.openmrs.mobile.application.OpenMRS;
 import org.openmrs.mobile.models.Location;
 import org.openmrs.mobile.models.retrofit.Results;
 import org.openmrs.mobile.utilities.ToastUtil;

14982c44837468c64e9a74fe57ce3b6a5c4c1f24
AdamGrzybkowski <agrzybkowski@outlook.com>
2016-10-27 11:23:40 +0200

    AC-221 catch NumberFormatException when parsing to double

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/fragments/FormPageFragment.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/fragments/FormPageFragment.java
@@ -190,7 +190,13 @@ public class FormPageFragment extends Fragment {
             if(!isEmpty(ed)) {
                 emp = false;
 
-                Double inp = Double.parseDouble(ed.getText().toString());
+                Double inp = null;
+                try {
+                    inp = Double.parseDouble(ed.getText().toString());
+                } catch (NumberFormatException e) {
+                    ToastUtil.error(e.getMessage());
+                    valid=false;
+                }
                 if (ed.getUpperlimit() != -1.0 && ed.getUpperlimit() != -1.0) {
                     if (ed.getUpperlimit() < inp || ed.getLowerlimit() > inp) {
                         ToastUtil.error("Value for " + ed.getName() + " is out of range. Value should be between "+

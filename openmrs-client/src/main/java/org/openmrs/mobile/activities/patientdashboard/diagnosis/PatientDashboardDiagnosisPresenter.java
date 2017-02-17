1262ef1ab6c0f2dcacc752f97ee5a87c3ad52175
AdamGrzybkowski <agrzybkowski@outlook.com>
2017-02-17 09:11:03 +0100

    AC-361 Unit test - PatientDashboardDiagnosisPresenter

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/diagnosis/PatientDashboardDiagnosisPresenter.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/diagnosis/PatientDashboardDiagnosisPresenter.java
@@ -21,6 +21,7 @@ import org.openmrs.mobile.dao.PatientDAO;
 import org.openmrs.mobile.models.Encounter;
 import org.openmrs.mobile.models.EncounterType;
 import org.openmrs.mobile.models.Observation;
+import org.openmrs.mobile.models.Patient;
 import org.openmrs.mobile.utilities.ApplicationConstants;
 
 import java.util.ArrayList;
@@ -31,13 +32,23 @@ import rx.android.schedulers.AndroidSchedulers;
 public class PatientDashboardDiagnosisPresenter extends PatientDashboardMainPresenterImpl implements PatientDashboardContract.PatientDiagnosisPresenter {
 
     private PatientDashboardContract.ViewPatientDiagnosis mPatientDiagnosisView;
+    private EncounterDAO encounterDAO;
 
 
     public PatientDashboardDiagnosisPresenter(String id,
                                             PatientDashboardContract.ViewPatientDiagnosis patientDiagnosisView) {
         this.mPatient = new PatientDAO().findPatientByID(id);
         this.mPatientDiagnosisView = patientDiagnosisView;
+        this.encounterDAO = new EncounterDAO();
         this.mPatientDiagnosisView.setPresenter(this);
+    }
+
+    public PatientDashboardDiagnosisPresenter(Patient patient,
+                                              PatientDashboardContract.ViewPatientDiagnosis mPatientDiagnosisView,
+                                              EncounterDAO encounterDAO) {
+        this.mPatient = patient;
+        this.mPatientDiagnosisView = mPatientDiagnosisView;
+        this.encounterDAO = encounterDAO;
     }
 
     private List<String> getAllDiagnosis(List<Encounter> encounters) {
@@ -62,13 +73,12 @@ public class PatientDashboardDiagnosisPresenter extends PatientDashboardMainPres
 
     @Override
     public void loadDiagnosis() {
-
-        addSubscription(new EncounterDAO().
-                getAllEncountersByType(mPatient.getId(), new EncounterType(EncounterType.VISIT_NOTE))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(encounters -> {
-                    mPatientDiagnosisView.setDiagnosesToDisplay(getAllDiagnosis(encounters));
-                }));
+        addSubscription(
+                encounterDAO.getAllEncountersByType(mPatient.getId(), new EncounterType(EncounterType.VISIT_NOTE))
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(encounters -> {
+                            mPatientDiagnosisView.setDiagnosesToDisplay(getAllDiagnosis(encounters));
+                        }));
     }
 
 }

25387736ab9b1fc2ba420a0e02ea039634827d55
Maharaj Fawwaz Almuqaddim Yusran <fawwazyusran@gmail.com>
2018-12-16 23:13:08 +0800

    AC-513: Move DateUtilsTest to a Unit test (#447)

--- openmrs-client/src/test/java/org/openmrs/mobile/utilities/DateUtilsTest.java
+++ openmrs-client/src/test/java/org/openmrs/mobile/utilities/DateUtilsTest.java
@@ -12,18 +12,40 @@
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
 
-package org.openmrs.mobile.test.unit;
-
-import android.test.InstrumentationTestCase;
+package org.openmrs.mobile.utilities;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
-import org.openmrs.mobile.utilities.DateUtils;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.openmrs.mobile.application.OpenMRS;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
-public class DateUtilsTest extends InstrumentationTestCase {
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(OpenMRS.class)
+public class DateUtilsTest {
+
+    private static final String TEST_TIMEZONE;
     private static final String INITIAL_DATA_CUSTOM_TIMEZONE;
     private static final String INITIAL_DATA_CUSTOM_FORMAT;
     private static final String INITIAL_DATA_CUSTOM_FORMAT_AND_TIMEZONE;
@@ -57,13 +79,8 @@ public class DateUtilsTest extends InstrumentationTestCase {
     private static final String INVALID_DATA_14;
     private static final String INVALID_DATA_15;
 
-    @Override
-    public void setUp() throws java.lang.Exception {
-        super.setUp();
-        getInstrumentation().waitForIdleSync();
-    }
-
     static {
+        TEST_TIMEZONE = "PST8PDT";
         INITIAL_DATA_CUSTOM_TIMEZONE = "1967-03-26T00:00:00.000+0200";
         EXPECTED_DATA_CUSTOM_TIMEZONE = "26/03/1967";
         INITIAL_DATA_CUSTOM_FORMAT = "1990-03-24T00:00";
@@ -71,11 +88,11 @@ public class DateUtilsTest extends InstrumentationTestCase {
         INITIAL_DATA_CUSTOM_FORMAT_AND_TIMEZONE = "1970-03-26T00:00:00.000+0200";
         EXPECTED_DATA_CUSTOM_FORMAT_AND_TIMEZONE = "1970-03-26T00:00";
         INITIAL_DATA_TIME_STRING = "1975-03-26T00:00:00.000+0200";
-        EXPECTED_DATA_TIME_STRING = "1975-03-25T15:00:00.000-07:00";
+        EXPECTED_DATA_TIME_STRING = "1975-03-25T15:00:00.000-0700";
         INITIAL_DATA_CONVERT_TIME_1 = "1980-03-26T00:00:00.000+0200";
         EXPECTED_DATA_CONVERT_TIME_1 = "25/03/1980";
         INITIAL_DATA_DATE_FROM_STRING = "1980-03-26T00:00:00.000+0200";
-        EXPECTED_DATA_DATE_FROM_STRING = "Wed Mar 26 00:00:00 PST 1980";
+        EXPECTED_DATA_DATE_FROM_STRING = "26/03/1980";
 
         EXPECTED_DATA_DEFAULT_DATE_FORMAT_1 = "26/03/1967";
         EXPECTED_DATA_DEFAULT_DATE_FORMAT_2 = "24/03/1990";
@@ -101,11 +118,29 @@ public class DateUtilsTest extends InstrumentationTestCase {
         INVALID_DATA_15 = "4/-2/1993";
     }
 
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private OpenMRS openMRS;
+
+    @Rule
+    public TimeZoneRule timeZoneRule = new TimeZoneRule(TimeZone.getTimeZone("PST8PDT"));
+
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(OpenMRS.class);
+        when(OpenMRS.getInstance()).thenReturn(openMRS);
+        Locale.setDefault(Locale.US);
+        TimeZone.setDefault(TimeZone.getTimeZone(TEST_TIMEZONE));
+    }
+
+    @Test
     public void testTimeConversion() {
         Long stringToLongResult;
         String longToStringResult;
         DateTime dateTime;
         String nullDateAsString = null;
+        DateTimeFormatter formatter = DateTimeFormat
+                .forPattern(DateUtils.OPEN_MRS_REQUEST_FORMAT)
+                .withZone(DateTimeZone.forID(TEST_TIMEZONE));
 
         stringToLongResult = DateUtils.convertTime(INITIAL_DATA_CUSTOM_TIMEZONE);
         longToStringResult = DateUtils.convertTime(stringToLongResult, TimeZone.getTimeZone("GMT+02:00"));
@@ -116,14 +151,14 @@ public class DateUtilsTest extends InstrumentationTestCase {
         assertEquals(EXPECTED_DATA_CUSTOM_FORMAT, longToStringResult);
 
         stringToLongResult = DateUtils.convertTime(INITIAL_DATA_CUSTOM_FORMAT_AND_TIMEZONE);
-        longToStringResult = DateUtils.convertTime(stringToLongResult,"yyyy-MM-dd'T'HH:mm",TimeZone.getTimeZone("GMT+02:00"));
-        assertEquals(EXPECTED_DATA_CUSTOM_FORMAT_AND_TIMEZONE,longToStringResult);
+        longToStringResult = DateUtils.convertTime(stringToLongResult, "yyyy-MM-dd'T'HH:mm", TimeZone.getTimeZone("GMT+02:00"));
+        assertEquals(EXPECTED_DATA_CUSTOM_FORMAT_AND_TIMEZONE, longToStringResult);
 
         dateTime = DateUtils.convertTimeString(INITIAL_DATA_TIME_STRING);
-        assertEquals(EXPECTED_DATA_TIME_STRING,String.valueOf(dateTime));
+        assertEquals(EXPECTED_DATA_TIME_STRING, formatter.print(dateTime));
 
-        longToStringResult = DateUtils.convertTime1(INITIAL_DATA_CONVERT_TIME_1,DateUtils.DEFAULT_DATE_FORMAT);
-        assertEquals(EXPECTED_DATA_CONVERT_TIME_1,longToStringResult);
+        longToStringResult = DateUtils.convertTime1(INITIAL_DATA_CONVERT_TIME_1, DateUtils.DEFAULT_DATE_FORMAT);
+        assertEquals(EXPECTED_DATA_CONVERT_TIME_1, longToStringResult);
 
         stringToLongResult = DateUtils.convertTime(INVALID_DATA_3);
         assertNotSame(EXPECTED_LONG_3, String.valueOf(stringToLongResult));
@@ -135,6 +170,7 @@ public class DateUtilsTest extends InstrumentationTestCase {
         assertNull(dateTime);
     }
 
+    @Test
     public void testDateValidation() {
         Date date;
         String nullDateAsString = null;
@@ -168,11 +204,13 @@ public class DateUtilsTest extends InstrumentationTestCase {
         assertFalse(DateUtils.validateDate(INVALID_DATA_14, date1900, date2000));
         assertFalse(DateUtils.validateDate(INVALID_DATA_15, date1900, date2000));
 
+        SimpleDateFormat formatter = new SimpleDateFormat(DateUtils.DEFAULT_DATE_FORMAT, Locale.US);
+
         date = DateUtils.getDateFromString(INITIAL_DATA_DATE_FROM_STRING);
-        assertEquals(EXPECTED_DATA_DATE_FROM_STRING, String.valueOf(date));
+        assertEquals(EXPECTED_DATA_DATE_FROM_STRING, formatter.format(date));
 
         date = DateUtils.getDateFromString(INITIAL_DATA_DATE_FROM_STRING, DateUtils.DATE_WITH_TIME_FORMAT);
-        assertEquals(EXPECTED_DATA_DATE_FROM_STRING,String.valueOf(date));
+        assertEquals(EXPECTED_DATA_DATE_FROM_STRING, formatter.format(date));
 
         // Null Date String
         date = DateUtils.getDateFromString(nullDateAsString, DateUtils.DATE_WITH_TIME_FORMAT);

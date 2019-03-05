9a3ec0415526672f5bb705c7a11472d71231a5f0
Rimjhim Bhadani <rimjhimbhadani@gmail.com>
2019-03-05 13:39:18 +0530

    AC-528: Warning dialog when back is pressed (#473)

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/addeditpatient/AddEditPatientContract.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/addeditpatient/AddEditPatientContract.java
@@ -44,6 +44,8 @@ public interface AddEditPatientContract {
         void startPatientDashbordActivity(Patient patient);
 
         void showUpgradeRegistrationModuleInfo();
+
+        boolean areFieldsNotEmpty();
     }
 
     interface Presenter extends BasePresenterContract {

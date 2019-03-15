4f27bfbc12f1b9cb4c1414e7f9835b13834e1bc7
shubham pinjwani <shubhampinjwani710@outlook.com>
2019-03-16 03:06:22 +0530

    strings.xml

--- openmrs-client/src/main/java/org/openmrs/mobile/api/retrofit/PatientApi.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/api/retrofit/PatientApi.java
@@ -16,6 +16,7 @@ package org.openmrs.mobile.api.retrofit;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 
 import com.activeandroid.query.Select;
 

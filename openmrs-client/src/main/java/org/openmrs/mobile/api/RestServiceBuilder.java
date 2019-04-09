95b6c4514528e3f579c9c6ea0408384ce890f6d5
Ram <tarekalabd1@gmail.com>
2019-04-09 15:12:08 +0200

    AC-534: Integrating android snooper for debugging purpose.

--- openmrs-client/src/main/java/org/openmrs/mobile/api/RestServiceBuilder.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/api/RestServiceBuilder.java
@@ -14,6 +14,7 @@ import android.util.Base64;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.prateekj.snooper.okhttp.SnooperInterceptor;
 
 import org.openmrs.mobile.application.OpenMRS;
 import org.openmrs.mobile.models.Observation;
@@ -71,6 +72,7 @@ public class RestServiceBuilder {
                 }
             });
             HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            httpClient.addInterceptor(new SnooperInterceptor());
             logging.setLevel(HttpLoggingInterceptor.Level.BODY);
             httpClient.addInterceptor(logging);
         }

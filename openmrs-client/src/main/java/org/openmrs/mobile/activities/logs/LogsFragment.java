152a882b05abf61c4e45d83a91dda57690e4789c
Vansh Arora <vansha10@gmail.com>
2019-04-24 13:06:56 +0530

    AC587: Obsolete SDK_INT Version Check (#557)

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/logs/LogsFragment.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/logs/LogsFragment.java
@@ -63,16 +63,10 @@ public class LogsFragment extends ACBaseFragment<LogsContract.Presenter> impleme
         });
     }
 
-    @SuppressWarnings("deprecation")
     private void setClipboard(Context context, String text) {
-        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB) {
-            android.text.ClipboardManager clipboard = (android.text.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-            clipboard.setText(text);
-        } else {
-            android.content.ClipboardManager clipboard = (android.content.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-            android.content.ClipData clip = android.content.ClipData.newPlainText("Copied Text", text);
-            clipboard.setPrimaryClip(clip);
-        }
+        android.content.ClipboardManager clipboard = (android.content.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        android.content.ClipData clip = android.content.ClipData.newPlainText("Copied Text", text);
+        clipboard.setPrimaryClip(clip);
     }
 
     public static LogsFragment newInstance() {

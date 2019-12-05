7a912ec0c572c7a0b936fe4967d4664a4faf8060
Roku234 <44003689+Roku234@users.noreply.github.com>
2019-12-04 22:46:15 -0500

    GCI-340: Fixed Static Field leak (#632)

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/diagnosis/PatientDiagnosisFragment.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/diagnosis/PatientDiagnosisFragment.java
@@ -38,6 +38,7 @@ import java.util.List;
 public class PatientDiagnosisFragment extends PatientDashboardFragment implements PatientDashboardContract.ViewPatientDiagnosis {
 
     private ListView mDiagnosisList;
+    private PatientDashboardActivity mPatientDashboardActivity;
 
     public static PatientDiagnosisFragment newInstance() {
         return new PatientDiagnosisFragment();
@@ -77,7 +78,7 @@ public class PatientDiagnosisFragment extends PatientDashboardFragment implement
         super.setUserVisibleHint(isVisibleToUser);
         if (isVisibleToUser) {
             try {
-                PatientDashboardActivity.hideFABs(true);
+                mPatientDashboardActivity.hideFABs(true);
             } catch (Exception e) {
                 e.printStackTrace();
             }

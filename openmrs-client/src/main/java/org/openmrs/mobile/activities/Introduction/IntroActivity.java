29f913c5c9f9546821e61e5d86f11bfee081cef9
rishabh-997 <rishabh.agarwal997@gmail.com>
2020-01-01 19:38:42 +0530

    resolved conflicts

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/Introduction/IntroActivity.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/Introduction/IntroActivity.java
@@ -37,12 +37,12 @@ public class IntroActivity extends AppIntro2
         super.onCreate(savedInstanceState);
 
         addSlide(AppIntroFragment.newInstance(getString(R.string.intro_welcome),"",getString(R.string.intro_welcome_desc),"", R.drawable.openmrs_logo, Color.parseColor("#ffffff"),Color.parseColor("#000000"),Color.parseColor("#000000")));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_register), getString(R.string.intro_register_desc), R.drawable.ico_registry, Color.parseColor("#5B57A6")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_register), getString(R.string.intro_register_desc), R.drawable.ico_registry, Color.parseColor("#000000")));
         addSlide(AppIntroFragment.newInstance(getString(R.string.intro_find), getString(R.string.intro_find_desc), R.drawable.ico_search, Color.parseColor("#009384")));
         addSlide(AppIntroFragment.newInstance(getString(R.string.intro_monitor), getString(R.string.intro_monitor_desc), R.drawable.ico_visits, Color.parseColor("#F0A815")));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_manage), getString(R.string.intro_manage_desc), R.mipmap.ic_provider, Color.parseColor("#F26522")));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_location), getString(R.string.intro_location_desc), R.drawable.ic_location, Color.parseColor("#009384")));
-        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_settings), getString(R.string.intro_settings_desc), R.mipmap.ic_privacy_policy, Color.parseColor("#F0A815")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_manage), getString(R.string.intro_manage_desc), R.drawable.ic_provider_big, Color.parseColor("#F26522")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_location), getString(R.string.intro_location_desc), R.drawable.ic_location_big, Color.parseColor("#009384")));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_settings), getString(R.string.intro_settings_desc), R.drawable.ic_settings_big, Color.parseColor("#F0A815")));
 
         if(!mOpenMRS.getFirstTime()) {
             startActivity(new Intent(this, DashboardActivity.class));

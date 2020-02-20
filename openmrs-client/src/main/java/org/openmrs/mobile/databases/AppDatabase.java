51c6f6a312c5b0ede004b763d991adf2f4ec3cba
rishabh-997 <rishabh.agarwal997@gmail.com>
2020-02-21 01:14:25 +0530

    resolvced conflicts

--- openmrs-client/src/main/java/org/openmrs/mobile/databases/AppDatabase.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/databases/AppDatabase.java
@@ -1,6 +1,24 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
 package org.openmrs.mobile.databases;
 
 import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import org.openmrs.mobile.dao.EncounterRoomDAO;
 import org.openmrs.mobile.dao.LocationRoomDAO;
 import org.openmrs.mobile.dao.VisitRoomDAO;
 import org.openmrs.mobile.dao.PatientRoomDAO;
@@ -11,10 +29,6 @@ import org.openmrs.mobile.databases.entities.ObservationEntity;
 import org.openmrs.mobile.databases.entities.PatientEntity;
 import org.openmrs.mobile.databases.entities.VisitEntity;
 import org.openmrs.mobile.utilities.ApplicationConstants;
-
-import androidx.room.Database;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
 
 @Database(entities = {ConceptEntity.class,
         EncounterEntity.class,
@@ -29,6 +43,7 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract LocationRoomDAO locationRoomDAO();
     public abstract VisitRoomDAO visitRoomDAO();
     public abstract PatientRoomDAO patientRoomDAO();
+    public abstract EncounterRoomDAO encounterRoomDAO();
 
     private static volatile AppDatabase INSTANCE;
 
@@ -44,4 +59,5 @@ public abstract class AppDatabase extends RoomDatabase {
         }
         return INSTANCE;
     }
+
 }

347fb089664f3199b3fdaac6602e55e942342045
rishabh-997 <rishabh.agarwal997@gmail.com>
2020-06-04 21:37:18 +0530

    Migrated Active Visits Package to Kotlin

---
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/activevisits/ActiveVisitPresenter.kt
@@ -0,0 +1,68 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.mobile.activities.activevisits
+
+import org.openmrs.mobile.R
+import org.openmrs.mobile.activities.BasePresenter
+import org.openmrs.mobile.dao.VisitDAO
+import org.openmrs.mobile.models.Visit
+import org.openmrs.mobile.utilities.FilterUtil
+import rx.android.schedulers.AndroidSchedulers
+
+class ActiveVisitPresenter : BasePresenter, ActiveVisitsContract.Presenter {
+    private var mActiveVisitsView: ActiveVisitsContract.View
+    private var visitDAO: VisitDAO
+
+    constructor(mActiveVisitsView: ActiveVisitsContract.View) {
+        this.mActiveVisitsView = mActiveVisitsView
+        this.mActiveVisitsView.setPresenter(this)
+        visitDAO = VisitDAO()
+    }
+
+    constructor(mActiveVisitsView: ActiveVisitsContract.View, visitDAO: VisitDAO) {
+        this.mActiveVisitsView = mActiveVisitsView
+        this.visitDAO = visitDAO
+        this.mActiveVisitsView.setPresenter(this)
+    }
+
+    override fun subscribe() {
+        updateVisitsInDatabaseList()
+    }
+
+    override fun updateVisitsInDatabaseList() {
+        mActiveVisitsView.setEmptyListText(R.string.search_visits_no_results)
+        addSubscription(visitDAO.activeVisits
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { visits: List<Visit?>? -> mActiveVisitsView.updateListVisibility(visits) },
+                        { mActiveVisitsView.setEmptyListText(R.string.search_visits_no_results) }
+                ))
+    }
+
+    override fun updateVisitsInDatabaseList(query: String?) {
+        mActiveVisitsView.setEmptyListText(R.string.search_patient_no_result_for_query, query)
+        addSubscription(visitDAO.activeVisits
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { visits: List<Visit?>? ->
+                            var visitList: List<Visit?>? = visits
+                            visitList = FilterUtil.getPatientsWithActiveVisitsFilteredByQuery(visits, query)
+                            mActiveVisitsView.updateListVisibility(visitList)
+                        },
+                        {
+                            mActiveVisitsView.setEmptyListText(R.string.search_patient_no_result_for_query, query)
+                        }
+                ))
+    }
+}
---
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/activevisits/ActiveVisitsActivity.kt
@@ -0,0 +1,70 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.mobile.activities.activevisits
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
+import org.openmrs.mobile.R
+import org.openmrs.mobile.activities.ACBaseActivity
+
+class ActiveVisitsActivity : ACBaseActivity() {
+    private var mPresenter: ActiveVisitsContract.Presenter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_active_visits)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            supportActionBar!!.elevation = 0f
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        }
+        // Create fragment
+        var activeVisitsFragment = supportFragmentManager.findFragmentById(R.id.activeVisitContentFrame) as ActiveVisitsFragment?
+        if (activeVisitsFragment == null) {
+            activeVisitsFragment = ActiveVisitsFragment.newInstance()
+        }
+        if (!activeVisitsFragment.isActive) {
+            addFragmentToActivity(supportFragmentManager,
+                    activeVisitsFragment, R.id.activeVisitContentFrame)
+        }
+        // Create the presenter
+        mPresenter = ActiveVisitPresenter(activeVisitsFragment)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.find_visits_menu, menu)
+        val findVisitView: SearchView
+        val mFindVisitItem = menu.findItem(R.id.actionSearchLocalVisits)
+        findVisitView = mFindVisitItem.actionView as SearchView
+
+        findVisitView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                findVisitView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+                if (query.isNotEmpty()) {
+                    mPresenter!!.updateVisitsInDatabaseList(query)
+                } else {
+                    mPresenter!!.updateVisitsInDatabaseList()
+                }
+                return true
+            }
+        })
+        return true
+    }
+}

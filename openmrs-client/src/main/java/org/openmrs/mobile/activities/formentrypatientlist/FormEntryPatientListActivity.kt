02cee5ee71d718cac0a0f7ab576022ac0c847f90
rishabh-997 <rishabh.agarwal997@gmail.com>
2020-06-13 12:31:01 +0530

    migrated FormEntryPatientList Package to kotlin

---
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/formentrypatientlist/FormEntryPatientListActivity.kt
@@ -0,0 +1,63 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.mobile.activities.formentrypatientlist
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
+import org.openmrs.mobile.R
+import org.openmrs.mobile.activities.ACBaseActivity
+
+class FormEntryPatientListActivity : ACBaseActivity() {
+    private var mPresenter: FormEntryPatientListContract.Presenter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.setContentView(R.layout.activity_form_entry_patient_list)
+
+        // Create fragment
+        var formEntryPatientListFragment = supportFragmentManager.findFragmentById(R.id.formEntryPatientListContentFrame) as FormEntryPatientListFragment?
+        if (formEntryPatientListFragment == null) {
+            formEntryPatientListFragment = FormEntryPatientListFragment.newInstance()
+        }
+        if (!formEntryPatientListFragment.isActive) {
+            addFragmentToActivity(supportFragmentManager,
+                    formEntryPatientListFragment, R.id.formEntryPatientListContentFrame)
+        }
+        // Create the presenter
+        mPresenter = FormEntryPatientListPresenter(formEntryPatientListFragment)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.form_entry_patient_list_menu, menu)
+        val mFindPatientMenuItem = menu.findItem(R.id.actionSearchRemoteFormEntry)
+        val findPatientView: SearchView = mFindPatientMenuItem.actionView as SearchView
+
+        // Search function
+        findPatientView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                findPatientView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+                mPresenter?.setQuery(query)
+                mPresenter?.updatePatientsList()
+                return true
+            }
+        })
+        return true
+    }
+}
---
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/formentrypatientlist/FormEntryPatientListAdapter.kt
@@ -0,0 +1,87 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.mobile.activities.formentrypatientlist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import org.openmrs.mobile.R
+import org.openmrs.mobile.dao.VisitDAO
+import org.openmrs.mobile.models.Patient
+import org.openmrs.mobile.models.Visit
+import org.openmrs.mobile.utilities.ApplicationConstants
+import org.openmrs.mobile.utilities.DateUtils
+import rx.android.schedulers.AndroidSchedulers
+
+class FormEntryPatientListAdapter(private val mContext: FormEntryPatientListFragment, private val mItems: List<Patient?>?) : RecyclerView.Adapter<FormEntryPatientListAdapter.PatientViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PatientViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.row_patient_details, parent, false)
+        return PatientViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: PatientViewHolder, position: Int) {
+        val adapterPos = holder.adapterPosition
+        val patient = this.mItems?.get(position)
+        VisitDAO().getActiveVisitByPatientId(patient?.id)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { visit: Visit? ->
+                    if (visit != null) {
+                        val icon = mContext.resources.getDrawable(R.drawable.active_visit_dot)
+                        icon.setBounds(0, 0, icon.intrinsicHeight, icon.intrinsicWidth)
+                        holder.mVisitStatus.setCompoundDrawables(icon, null, null, null)
+                        holder.mVisitStatus.text = mContext.getString(R.string.active_visit_label_capture_vitals)
+                        holder.mRowLayout.setOnClickListener { mContext.startEncounterForPatient(mItems?.get(adapterPos)?.id) }
+                    } else {
+                        holder.mVisitStatus.text = ApplicationConstants.EMPTY_STRING
+                        holder.mRowLayout.setOnClickListener { v: View? -> mContext.showSnackbarInactivePatients(v) }
+                    }
+                }
+        if (null != patient?.identifier) {
+            val display = "#" + patient.identifier.identifier
+            holder.mIdentifier.text = display
+        }
+        if (null != patient?.name) {
+            holder.mDisplayName.text = patient.name.nameString
+        }
+        if (null != patient?.gender) {
+            holder.mGender.text = patient.gender
+        }
+        holder.mBirthDate.text = DateUtils.convertTime(DateUtils.convertTime(patient?.birthdate))
+    }
+
+    override fun onViewDetachedFromWindow(holder: PatientViewHolder) {
+        holder.clearAnimation()
+    }
+
+    override fun getItemCount(): Int {
+        return mItems?.size!!
+    }
+
+    class PatientViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val mRowLayout: CardView = itemView as CardView
+        val mIdentifier: TextView = itemView.findViewById(R.id.syncedPatientIdentifier)
+        val mDisplayName: TextView = itemView.findViewById(R.id.syncedPatientDisplayName)
+        val mGender: TextView = itemView.findViewById(R.id.syncedPatientGender)
+        val mBirthDate: TextView = itemView.findViewById(R.id.syncedPatientBirthDate)
+        val mVisitStatus: TextView = itemView.findViewById(R.id.visitStatusLabel)
+
+        fun clearAnimation() {
+            mRowLayout.clearAnimation()
+        }
+    }
+
+}

87e2a95b1e345bee84fd721acba5e7cac5337e7e
Rishabh Agarwal <45125121+rishabh-997@users.noreply.github.com>
2020-06-19 20:47:09 +0530

    AC-776 Migrated Logs activity to kotlin (#766)

---
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/logs/LogsFragment.kt
@@ -0,0 +1,61 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.mobile.activities.logs
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import org.openmrs.mobile.R
+import org.openmrs.mobile.activities.ACBaseFragment
+import org.openmrs.mobile.databinding.FragmentLogsBinding
+
+class LogsFragment : ACBaseFragment<LogsContract.Presenter>(), LogsContract.View {
+
+    private lateinit var _binding: FragmentLogsBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        _binding = FragmentLogsBinding.inflate(inflater, container, false)
+        return _binding.root
+    }
+
+    override fun attachLogsToTextView(logs: String?) {
+        _binding.tvLogs.text = logs
+    }
+
+    override fun fabCopyAll(textLogs: String?) {
+        _binding.fab.setOnClickListener {
+            setClipboard(context, textLogs)
+            Toast.makeText(context, R.string.logs_copied_to_clipboard_message,
+                    Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setClipboard(context: Context?, text: String?) {
+        val clipboard = context!!.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText(getString(R.string.copied_text), text)
+        clipboard.primaryClip = clip
+    }
+
+    companion object {
+        fun newInstance(): LogsFragment {
+            return LogsFragment()
+        }
+    }
+}
---
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/logs/LogsPresenter.kt
@@ -0,0 +1,52 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.mobile.activities.logs
+
+import org.openmrs.mobile.activities.BasePresenter
+import org.openmrs.mobile.application.OpenMRS
+import org.openmrs.mobile.application.OpenMRSLogger
+import java.io.*
+
+class LogsPresenter(private val mLogsView: LogsContract.View, private val mOpenMRSLogger: OpenMRSLogger) : BasePresenter(), LogsContract.Presenter {
+
+    override fun subscribe() {
+        val logsText = getLogs()
+        mLogsView.attachLogsToTextView(logsText)
+        mLogsView.fabCopyAll(logsText)
+    }
+
+    init {
+        mLogsView.setPresenter(this)
+    }
+
+    private fun getLogs(): String {
+        var textLogs = ""
+        val filename = (OpenMRS.getInstance().openMRSDir
+                + File.separator + mOpenMRSLogger.logFilename)
+        try {
+            val myFile = File(filename)
+            val fIn = FileInputStream(myFile)
+            val myReader = BufferedReader(InputStreamReader(fIn))
+            while (myReader.readLine() != null) {
+                textLogs += myReader.readLine()
+            }
+            myReader.close()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return textLogs
+    }
+}

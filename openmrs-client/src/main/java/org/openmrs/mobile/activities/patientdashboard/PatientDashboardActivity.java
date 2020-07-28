728bc296d337cd4d913faf71e880986db39733b6
rishabh-997 <rishabh.agarwal997@gmail.com>
2020-07-28 12:26:22 +0530

    Aligned theme to material UI

--- openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/PatientDashboardActivity.java
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/patientdashboard/PatientDashboardActivity.java
@@ -55,6 +55,7 @@ import org.openmrs.mobile.activities.patientdashboard.vitals.PatientVitalsFragme
 import org.openmrs.mobile.utilities.ApplicationConstants;
 import org.openmrs.mobile.utilities.ImageUtils;
 import org.openmrs.mobile.utilities.TabUtil;
+import org.openmrs.mobile.utilities.ThemeUtils;
 
 public class PatientDashboardActivity extends ACBaseActivity {
     private String mId;
@@ -120,6 +121,9 @@ public class PatientDashboardActivity extends ACBaseActivity {
     private void initViewPager(PatientDashboardPagerAdapter adapter) {
         final ViewPager viewPager = findViewById(R.id.pager);
         TabLayout tabHost = findViewById(R.id.tabhost);
+        if(ThemeUtils.isDarkModeActivated()) {
+            tabHost.setBackgroundColor(getResources().getColor(R.color.black));
+        }
         viewPager.setOffscreenPageLimit(adapter.getCount() - 1);
         viewPager.setAdapter(adapter);
         tabHost.setupWithViewPager(viewPager);

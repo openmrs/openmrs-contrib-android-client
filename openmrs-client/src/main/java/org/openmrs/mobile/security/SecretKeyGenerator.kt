41b58f7f810561c88010ad5e2674b92036ba798c
Avinash Vijayvargiya <avinash14022002@gmail.com>
2021-03-11 16:56:13 +0530

    Converted: SecretKeyGenerator.java to SecretKeyGenerator.kt

--- openmrs-client/src/main/java/org/openmrs/mobile/security/SecretKeyGenerator.kt
+++ openmrs-client/src/main/java/org/openmrs/mobile/security/SecretKeyGenerator.kt
@@ -11,53 +11,46 @@
  *
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
+package org.openmrs.mobile.security
 
-package org.openmrs.mobile.security;
+import org.openmrs.mobile.application.OpenMRS
+import java.security.NoSuchAlgorithmException
+import java.security.SecureRandom
+import javax.crypto.KeyGenerator
+import kotlin.experimental.and
 
-import org.openmrs.mobile.application.OpenMRS;
-
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-
-public final class SecretKeyGenerator {
-    private SecretKeyGenerator() {
-    }
-
-    public static String generateKey() {
+object SecretKeyGenerator {
+    fun generateKey(): String? {
         // Generate a 256-bit key
-        final int outputKeyLength = 256;
-        KeyGenerator keyGenerator = null;
-        SecureRandom secureRandom = null;
+        val outputKeyLength = 256
+        var keyGenerator: KeyGenerator? = null
+        var secureRandom: SecureRandom? = null
         try {
-            secureRandom = SecureRandom.getInstance("SHA1PRNG");
+            secureRandom = SecureRandom.getInstance("SHA1PRNG")
             // Do *not* seed secureRandom! Automatically seeded from system entropy.
-            keyGenerator = KeyGenerator.getInstance("AES");
-        } catch (NoSuchAlgorithmException e) {
-            OpenMRS.getInstance().getOpenMRSLogger().d("Failed to generate DB secret key" + e.toString());
+            keyGenerator = KeyGenerator.getInstance("AES")
+        } catch (e: NoSuchAlgorithmException) {
+            OpenMRS.getInstance().openMRSLogger.d("Failed to generate DB secret key$e")
         }
-        keyGenerator.init(outputKeyLength, secureRandom);
-        SecretKey key = keyGenerator.generateKey();
-        return bytesToHex(key.getEncoded());
+        keyGenerator!!.init(outputKeyLength, secureRandom)
+        val key = keyGenerator.generateKey()
+        return bytesToHex(key.encoded)
     }
 
-    private static String bytesToHex(byte[] data) {
+    private fun bytesToHex(data: ByteArray?): String? {
         if (data == null) {
-            return null;
+            return null
         }
-
-        int len = data.length;
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < len; i++) {
-            if ((data[i] & 0xFF) < 16) {
-                stringBuilder.append('0');
-                stringBuilder.append(java.lang.Integer.toHexString(data[i] & 0xFF));
+        val len = data.size
+        val stringBuilder = StringBuilder()
+        for (i in 0 until len) {
+            if (data[i] and 0xFF.toByte() < 16) {
+                stringBuilder.append('0')
+                stringBuilder.append(Integer.toHexString((data[i] and 0xFF.toByte()).toInt()))
             } else {
-                stringBuilder.append(java.lang.Integer.toHexString(data[i] & 0xFF));
+                stringBuilder.append(Integer.toHexString((data[i] and 0xFF.toByte()).toInt()))
             }
         }
-        return stringBuilder.toString();
+        return stringBuilder.toString()
     }
 }

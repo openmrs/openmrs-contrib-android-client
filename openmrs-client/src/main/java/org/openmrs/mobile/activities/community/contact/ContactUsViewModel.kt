9148d0388611cf5603fbf28acf01133c8ac9cc0a
Amr Salaheddin Abdelhady <52531091+amrsalah3@users.noreply.github.com>
2022-09-12 04:37:25 +0200

    AC-1039: Migrate community.contact package from MVP to MVVM architecture
    Signed-off-by: amrsalah3 <amrhady3@gmail.com>

---
+++ openmrs-client/src/main/java/org/openmrs/mobile/activities/community/contact/ContactUsViewModel.kt
@@ -0,0 +1,8 @@
+package org.openmrs.mobile.activities.community.contact
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.openmrs.mobile.activities.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ContactUsViewModel @Inject constructor() : BaseViewModel<Unit>()

edcdb10faeabe06fce7f77e56ae4bc4b6b95f222
shubham sharma <shubham.sharma19@bajajfinserv.in>
2023-07-18 02:39:36 +0530

    AC-1088: Added more tests for AppointmentRepository

--- openmrs-android-sdk/src/test/java/com/openmrs/android_sdk/library/api/repository/AppointmentRepositoryTest.kt
+++ openmrs-android-sdk/src/test/java/com/openmrs/android_sdk/library/api/repository/AppointmentRepositoryTest.kt
@@ -1,19 +1,11 @@
 package com.openmrs.android_sdk.library.api.repository
 
-import com.openmrs.android_sdk.library.OpenmrsAndroid
 import com.openmrs.android_sdk.library.api.RestApi
-import com.openmrs.android_sdk.library.databases.AppDatabase
-import com.openmrs.android_sdk.library.databases.AppDatabaseHelper
 import com.openmrs.android_sdk.library.databases.entities.LocationEntity
-import com.openmrs.android_sdk.library.models.AppointmentBlock
-import com.openmrs.android_sdk.library.models.AppointmentType
-import com.openmrs.android_sdk.library.models.Provider
-import com.openmrs.android_sdk.library.models.VisitType
+import com.openmrs.android_sdk.library.models.*
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.HiltTestApplication
-import io.mockk.every
-import io.mockk.mockk
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okio.buffer
@@ -28,20 +20,12 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import rx.Observable
-import rx.schedulers.Schedulers
-import java.util.concurrent.Callable
 import javax.inject.Inject
 
 @HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
 @Config(application = HiltTestApplication::class)
 class AppointmentRepositoryTest {
-
-    fun createObservableIOForTest(func: Callable<AppointmentBlock>): Observable<AppointmentBlock> {
-        return Observable.fromCallable(func)
-            .subscribeOn(Schedulers.io())
-    }
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
@@ -77,6 +61,55 @@ class AppointmentRepositoryTest {
         }
     )
 
+    val appointmentBlockGet = AppointmentBlock().apply {
+        startDate = "2023-07-15T10:00:00Z"
+        endDate = "2023-07-15T11:00:00Z"
+        provider = Provider().apply {
+            identifier = "user1"
+            retired = false
+        }
+        location = LocationEntity("Sample Clinic").apply {
+            name = "Sample Clinic"
+            address_1 = "123 Main St"
+        }
+        types = listOf(
+            AppointmentType().apply {
+                visitType = VisitType().apply {
+                    uuid = "12345678-1234-1234-1234-123456789abc"
+                    display = "Type A"
+                }
+                description = "Sample description 1"
+                duration = "30 minutes"
+                confidential = false
+            },
+            AppointmentType().apply {
+                visitType = VisitType().apply {
+                    uuid = "98765432-5432-5432-5432-987654321fed"
+                    display = "Type B"
+                }
+                description = "Sample description 2"
+                duration = "60 minutes"
+                confidential = true
+            }
+        )
+    }
+
+    val timeSlotGet = TimeSlot().apply {
+        startDate = "2023-07-15T10:00:00Z"
+        endDate = "2023-07-15T11:00:00Z"
+        appointmentBlock = appointmentBlockGet
+    }
+
+    val appointmentType = AppointmentType().apply {
+        visitType = VisitType().apply {
+            uuid = "12345678-1234-1234-1234-123456789abc"
+            display = "Type A"
+        }
+        description = "Sample description 1"
+        duration = "30 minutes"
+        confidential = false
+    }
+
     @Before
     fun setup() {
         hiltRule.inject()
@@ -90,7 +123,6 @@ class AppointmentRepositoryTest {
 
         appointmentApi = retrofit.create(RestApi::class.java)
         appointmentRepository.restApi = appointmentApi
-        //every { AppDatabase.getDatabase(any()) } returns mockk(relaxed = true)
     }
 
     @After
@@ -101,43 +133,32 @@ class AppointmentRepositoryTest {
     @Test
     fun createAppointmentBlock_success_returnsAppointmentBlock() {
 
-        val appointmentBlockGet = AppointmentBlock().apply {
-            startDate = "2023-07-15T10:00:00Z"
-            endDate = "2023-07-15T11:00:00Z"
-            provider = Provider().apply {
-                identifier = "user1"
-                retired = false
-            }
-            location = LocationEntity("Sample Clinic").apply {
-                name = "Sample Clinic"
-                address_1 = "123 Main St"
-            }
-            types = listOf(
-                AppointmentType().apply {
-                    visitType = VisitType().apply {
-                        uuid = "12345678-1234-1234-1234-123456789abc"
-                        display = "Type A"
-                    }
-                    description = "Sample description 1"
-                    duration = "30 minutes"
-                    confidential = false
-                },
-                AppointmentType().apply {
-                    visitType = VisitType().apply {
-                        uuid = "98765432-5432-5432-5432-987654321fed"
-                        display = "Type B"
-                    }
-                    description = "Sample description 2"
-                    duration = "60 minutes"
-                    confidential = true
-                }
-            )
-        }
-
-        enqueueMockResponse("mocked_responses/AppointmentRepository/AppointmentCreateBlock-success.json")
+        enqueueMockResponse("mocked_responses/AppointmentRepository/AppointmentBlockCreate-success.json")
         val result: AppointmentBlock = appointmentRepository.createAppointmentBlock(startDate, endDate, location, appointmentTypesList).toBlocking().first()
 
         assertEquals(result.endDate, appointmentBlockGet.endDate)
+
+    }
+
+    @Test
+    fun createTimeSlot_success_returnsTimeSlot() {
+
+        enqueueMockResponse("mocked_responses/AppointmentRepository/TimeSlotCreate-success.json")
+        val result: TimeSlot = appointmentRepository.createTimeSlot(timeSlotGet).toBlocking().first()
+
+        assertEquals(result.endDate, timeSlotGet.endDate)
+    }
+
+    @Test
+    fun createTimeSlotWithQuery_success_returnsTimeSlot() {
+        val startDate = "2023-07-15T10:00:00Z"
+        val endDate = "2023-07-15T11:00:00Z"
+        val appointmentBlockGet = appointmentBlockGet
+
+        enqueueMockResponse("mocked_responses/AppointmentRepository/TimeSlotCreate-success.json")
+        val result: TimeSlot = appointmentRepository.createTimeSlot(startDate, endDate, appointmentBlockGet).toBlocking().first()
+
+        assertEquals(result.endDate, endDate)
 
     }
 
